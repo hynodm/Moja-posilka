cedde15a71972e3742137a67f0dcbd27192c18ec
hynodm <hynodm@gmail.com>
2026-08-15 14:52:56 +0200

    Remove extra line in app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.set_page_config(page_title="Gym Progres", layout="wide", page_icon="🏋️")
 # --- 2. KONFIGURÁCIA GOOGLE FORMULÁRA ---
 # Adresa pre odosielanie dát z formulára
 FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSe_bSMHDGEvmPZUP4ZBQ2nq-Yos_30Zww5jLe9ZKzjgQ/formResponse"
-
 # Mapovanie ID políčok z tvojho formulára
 ENTRY_DATUM = "entry.1160346068"
 ENTRY_KATEGORIA = "entry.312830153"
